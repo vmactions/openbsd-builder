ae98207220797886b8f8da35de5048b8f7dc046f
neil <gitpc@neilpang.com>
2024-10-12 19:16:52 +0200

    fix

--- hooks/postBuild.sh
+++ hooks/postBuild.sh
@@ -7,6 +7,14 @@ echo 'pkg_scripts=""' >>/etc/rc.conf.local
 
 syspatch
 
+ret="$?"
+#0 means ok
+#2 means no update
+if [ "$ret" != "2" ] && [ "$ret" != "0" ]; then
+  echo "update error"
+  exit $ret
+fi
+
 
 
 
