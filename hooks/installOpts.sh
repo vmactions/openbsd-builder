833ad8c2244b0550299e82a6ce9142f18f96a096
neil <gitpc@neilpang.com>
2025-01-05 21:45:28 +0100

    s

--- hooks/installOpts.sh
+++ hooks/installOpts.sh
@@ -15,7 +15,7 @@ waitForText "nstall or"
 $vmsh string i
 $vmsh enter
 
-if [ "$VM_ARCH" = "aarch64" ] && [ "$VM_USE_CONSOLE_BUILD" ] && [ "$VM_USE_CONSOLE_BUILD_SSH" ]; then
+if [ "$VM_ARCH" = "aarch64" ] && [ "$VM_USE_CONSOLE_BUILD" ]; then
   #for openbsd 7.3/4,  it will reboot after the install.
   #but for 7.5/6, it will just shutdown after install,  so we force it to shutdown for 7.3/4 here.
   waitForText "Your OpenBSD install has been successfully completed"
