91f5346d2ffcafb3ff1df09384fbf10b0130301f
neil <gitpc@neilpang.com>
2025-01-07 19:07:06 +0100

    s

--- hooks/postBuild.sh
+++ hooks/postBuild.sh
@@ -4,6 +4,13 @@
 echo 'pkg_scripts=""' >>/etc/rc.conf.local
 
 
+while ps aux | grep "[m]ake newbsd"; do
+  echo "reorder_kernel is running, just wait"
+  sleep 5
+done
+
+echo "OK, start syspatch"
+sleep 10
 
 syspatch
 
@@ -12,6 +19,7 @@ ret="$?"
 #2 means no update
 if [ "$ret" != "2" ] && [ "$ret" != "0" ]; then
   echo "update error"
+  ps aux
   exit $ret
 fi
 
