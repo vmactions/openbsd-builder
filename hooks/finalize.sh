9a4ab91dbdf4a507034cfb13dd1de01d7c9c7cd5
neil <gitpc@neilpang.com>
2025-01-12 13:29:40 +0100

    enable root autologin

---
+++ hooks/finalize.sh
@@ -0,0 +1,13 @@
+
+
+
+#remove root password
+sed -i 's|$2b$10$qS3/zFLn/6wTQrjNhAddEepvKw.XculyRsXH60FLXjcj5fQeZzIQu||' /etc/master.passwd
+pwd_mkdb -p /etc/master.passwd
+
+
+#enable autologin with root in the console
+echo "su - root" >>/etc/rc.local
+
+
+
