52fc4650960399d53933539a43a58556efbfeb58
Neilpang <Neilpang@users.noreply.github.com>
2025-04-06 20:12:35 +0000

    Update from base builder

--- build.sh
+++ build.sh
@@ -305,8 +305,20 @@ if [ -e "hooks/postBuild.sh" ]; then
   # Reboot here, possible there were system updates done that need
   # a reboot to take effect before more operations can be done
   restart_and_wait
-fi
 
+  #wait for the sshd to start
+  _retry=0
+  while ! timeout 2 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR $osname exit >/dev/null 2>&1; do
+    echo "ssh is not ready, just wait."
+    sleep 10
+    _retry=$(($_retry + 1))
+    if [ $_retry -gt 10 ]; then
+      echo "ssh is failed."
+      exit 1
+    fi
+  done
+
+fi
 
 output="$osname-$VM_RELEASE"
 if [ "$VM_ARCH" ]; then
