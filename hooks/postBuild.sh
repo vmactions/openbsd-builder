13e0c1fdeb04431b361fe0981884e479b17d2cfc
neil <gitpc@neilpang.com>
2025-11-08 10:30:04 +0100

    fix for riscv64

--- hooks/postBuild.sh
+++ hooks/postBuild.sh
@@ -3,26 +3,30 @@
 
 echo 'pkg_scripts=""' >>/etc/rc.conf.local
 
-sleep 20
-while ps aux | grep "[m]ake new"; do
-  echo "reorder_kernel is running, just wait"
-  sleep 5
-done
 
-echo "OK, start syspatch"
-
-syspatch
-syspatch
-
-ret="$?"
-#0 means ok
-#2 means no update
-if [ "$ret" != "2" ] && [ "$ret" != "0" ]; then
-  echo "update error"
-  ps aux
-  exit $ret
+#openbsd doesn't support syspatch for riscv64
+#https://cdn.openbsd.org/pub/OpenBSD/syspatch/7.8/
+if [ "$(uname -m)" != "riscv64" ]; then
+  sleep 20
+  while ps aux | grep "[m]ake new"; do
+    echo "reorder_kernel is running, just wait"
+    sleep 5
+  done
+  
+  echo "OK, start syspatch"
+  
+  syspatch
+  syspatch
+  
+  ret="$?"
+  #0 means ok
+  #2 means no update
+  if [ "$ret" != "2" ] && [ "$ret" != "0" ]; then
+    echo "update error"
+    ps aux
+    exit $ret
+  fi
 fi
-
 
 
 
